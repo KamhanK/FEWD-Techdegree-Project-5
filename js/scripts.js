e96f48d18780d2bb52325db61a4e2d8f9d37090a
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-01-27 13:46:34 -0500

    Updated HTML, CSS & JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,24 @@
+/* Search Code */
+
+
+
+/* Lightbox Code */
+
 lightbox.option({
     'showImageNumberLabel': false,
-    'maxWidth': 800,
+    'maxWidth': 750,
+    'positionFromTop': 210,
     'wrapAround': true
+    
 })
+
+
+
+
+
+
+
+
+
+
+
