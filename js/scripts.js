57c862d8566dcd886a29162fbf093b2d6a4ad126
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-02-08 21:20:33 -0500

    Updated JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,24 @@
 /* Search Code */
 
+// const attrData = $('a').attr('data-title');
+// console.log(attrData);
 
+
+const searchString = document.getElementById('search');
+
+addEventListener('keyup', logKey);
+function logKey(event) {
+    const attrSearchString = searchString.getAttribute('data-title');
+    if ( attrSearchString === searchString ) {
+    console.log(searchString.value.toLowerCase());
+    }
+}
+
+
+// document.getElementById('search').addEventListener('keyup', searchFunction);
+// const searchString = document.getElementById('search');
+// searchString.value = searchString.value.toLowerCase();
+// console.log(searchString);
 
 /* Lightbox Code */
 
