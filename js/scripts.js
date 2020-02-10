d812df03561a23927154c8c3915877ef60a7bfab
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-02-10 13:32:57 -0500

    Updated JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,20 +1,29 @@
 /* Search Code */
 
-const searchString = document.getElementById('search');
+$(document).ready(function(){
+    $("#search").on("keyup", function() {
+      var value = $(this).val().toLowerCase();
+      $('a').attr('data-title').filter(function() {
+        $(this).toggle($(this).text().toLowerCase().indexOf(value) > -1)
+      });
+    });
+  });
 
-searchString.addEventListener('keyup', () => {
-    const input = searchString.value.toLowerCase();
-    const a = document.getElementsByClassName('item');
-    // console.log(input);
-    for (let i = 0; i < a.length; i += 1) {
-        const attrData = a[i].getAttribute('data-title');
-        if (attrData.toLowerCase().indexOf(input) > -1) {
-            a[i].style.display = "";
-            } else {
-            a[i].style.display = "none";
-            }
-        }
-});
+// const searchString = document.getElementById('search');
+
+// searchString.addEventListener('keyup', () => {
+//     const input = searchString.value.toLowerCase();
+//     const a = document.getElementsByClassName('item');
+//     // console.log(input);
+//     for (let i = 0; i < a.length; i += 1) {
+//         const attrData = a[i].getAttribute('data-title');
+//         if (attrData.toLowerCase().indexOf(input) > -1) {
+//             a[i].style.display = "";
+//             } else {
+//             a[i].style.display = "none";
+//             }
+//         }
+// });
 
 /* Lightbox Code */
 
