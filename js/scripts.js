4053c88933226d9a96b0c99cb7a79386b5d9c6ab
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-02-10 13:04:07 -0500

    Updated HTML, CSS, JS

--- js/scripts.js
+++ js/scripts.js
@@ -1,18 +1,14 @@
 /* Search Code */
 
-// const attrData = $('a').attr('data-title');
-// console.log(attrData);
-
-
 const searchString = document.getElementById('search');
 
 searchString.addEventListener('keyup', () => {
     const input = searchString.value.toLowerCase();
-    const a = document.getElementsByTagName('a');
+    const a = document.getElementsByClassName('item');
     // console.log(input);
     for (let i = 0; i < a.length; i += 1) {
         const attrData = a[i].getAttribute('data-title');
-        if (attrData.indexOf(input) > -1) {
+        if (attrData.toLowerCase().indexOf(input) > -1) {
             a[i].style.display = "";
             } else {
             a[i].style.display = "none";
@@ -20,30 +16,10 @@ searchString.addEventListener('keyup', () => {
         }
 });
 
-// function myFunction() {
-//     // Declare variables
-//     var input, filter, ul, li, a, i, txtValue;
-//     input = document.getElementById('myInput');
-//     filter = input.value.toUpperCase();
-//     ul = document.getElementById("myUL");
-//     li = ul.getElementsByTagName('li');
-  
-//     // Loop through all list items, and hide those who don't match the search query
-//     for (i = 0; i < li.length; i++) {
-//       a = li[i].getElementsByTagName("a")[0];
-//       txtValue = a.textContent || a.innerText;
-//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
-//         li[i].style.display = "";
-//       } else {
-//         li[i].style.display = "none";
-//       }
-//     }
-//   }
-
 /* Lightbox Code */
 
 lightbox.option({
-    'alwaysShowNavOnTouchDevices': false,
+    'alwaysShowNavOnTouchDevices': true,
     'showImageNumberLabel': false,
     'maxWidth': 750,
     'positionFromTop': 210,
