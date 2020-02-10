c0d551b55238384df22ba8440e765b270d556783
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-02-10 10:12:17 -0500

    Updated JS

--- js/scripts.js
+++ js/scripts.js
@@ -7,39 +7,25 @@
 const searchString = document.getElementById('search');
 const attrSearchString = document.getElementsByTagName('a')
 var captions = [
-    
     'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
-    
-    'The lake was so calm today. We had a great view of the snow on the mountains from here.',
-    
+    'The lake was so calm today. We had a great view of the snow on the mountains from here.', 
     'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
-    
     'It was amazing to see an iceberg up close, it was so cold but didn’t snow today.',
-   
     'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
-    
     'Fall is coming, I love when the leaves on the trees start to change color.',
-    
     'I drove past this plantation yesterday, everything is so green!',
-    
     'My summer vacation to the Oregon Coast. I love the sandy dunes!',
-    
     'We enjoyed a quiet stroll down this countryside lane.',
-    
     'Sunset at the coast! The sky turned a lovely shade of orange.',
-    
     'I did a tour of a cave today and the view of the landscape below was breathtaking.',
-    
     'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.'
-
     ];
 
 
-addEventListener('keyup', logKey);
-function logKey(event) {
+searchString.addEventListener('keyup', () => {
+    searchString.value.toLowerCase();
     console.log(searchString.value.toLowerCase());
-    
-}
+});
 
 for (let i = 0; i < attrSearchString.length; i += 1) {
     const attrData = attrSearchString[i].getAttribute('data-title');
