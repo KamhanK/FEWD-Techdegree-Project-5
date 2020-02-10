3befe61db1871a6a5adb82b77209ce02a2fde073
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-02-10 11:19:08 -0500

    Updated JS

--- js/scripts.js
+++ js/scripts.js
@@ -5,36 +5,45 @@
 
 
 const searchString = document.getElementById('search');
-const attrSearchString = document.getElementsByTagName('a')
-var captions = [
-    'I love hay bales. Took this snap on a drive through the countryside past some straw fields.',
-    'The lake was so calm today. We had a great view of the snow on the mountains from here.', 
-    'I hiked to the top of the mountain and got this picture of the canyon and trees below.',
-    'It was amazing to see an iceberg up close, it was so cold but didn’t snow today.',
-    'The red cliffs were beautiful. It was really hot in the desert but we did a lot of walking through the canyons.',
-    'Fall is coming, I love when the leaves on the trees start to change color.',
-    'I drove past this plantation yesterday, everything is so green!',
-    'My summer vacation to the Oregon Coast. I love the sandy dunes!',
-    'We enjoyed a quiet stroll down this countryside lane.',
-    'Sunset at the coast! The sky turned a lovely shade of orange.',
-    'I did a tour of a cave today and the view of the landscape below was breathtaking.',
-    'I walked through this meadow of bluebells and got a good view of the snow on the mountain before the fog came in.'
-    ];
-
 
 searchString.addEventListener('keyup', () => {
-    searchString.value.toLowerCase();
-    console.log(searchString.value.toLowerCase());
+    const input = searchString.value.toLowerCase();
+    const a = document.getElementsByTagName('a');
+    // console.log(input);
+    for (let i = 0; i < a.length; i += 1) {
+        const attrData = a[i].getAttribute('data-title');
+        if (attrData.indexOf(input) > -1) {
+            a[i].style.display = "";
+            } else {
+            a[i].style.display = "none";
+            }
+        }
 });
 
-for (let i = 0; i < attrSearchString.length; i += 1) {
-    const attrData = attrSearchString[i].getAttribute('data-title');
-    console.log(attrData);
-}
+// function myFunction() {
+//     // Declare variables
+//     var input, filter, ul, li, a, i, txtValue;
+//     input = document.getElementById('myInput');
+//     filter = input.value.toUpperCase();
+//     ul = document.getElementById("myUL");
+//     li = ul.getElementsByTagName('li');
+  
+//     // Loop through all list items, and hide those who don't match the search query
+//     for (i = 0; i < li.length; i++) {
+//       a = li[i].getElementsByTagName("a")[0];
+//       txtValue = a.textContent || a.innerText;
+//       if (txtValue.toUpperCase().indexOf(filter) > -1) {
+//         li[i].style.display = "";
+//       } else {
+//         li[i].style.display = "none";
+//       }
+//     }
+//   }
 
 /* Lightbox Code */
 
 lightbox.option({
+    'alwaysShowNavOnTouchDevices': false,
     'showImageNumberLabel': false,
     'maxWidth': 750,
     'positionFromTop': 210,
