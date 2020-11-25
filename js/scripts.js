c6ee97113622835c38cfe86bad1b162436931756
Shoaib Kamal Khan <shoaibkkhan@hotmail.com>
2020-11-25 08:24:39 -0500

    Updated comments for JS

--- js/scripts.js
+++ js/scripts.js
@@ -3,6 +3,8 @@
 const searchString = document.getElementById('search');
 const a = document.getElementsByClassName('item');
 
+/* This event listener listens for any keyup events in the input search field and displays the resulting thumbnails of the search. */
+
 searchString.addEventListener('keyup', () => {
     const input = searchString.value.toLowerCase();
 
@@ -16,13 +18,15 @@ searchString.addEventListener('keyup', () => {
     }
 });
 
+/* This event listener listens for a null value in the input search field so the search results are reset and all thumbnails are displayed. */
+
 searchString.addEventListener('search', () => {
     if (event.target.value === '') {
       for (let i = 0; i < a.length; i += 1) {
         a[i].style.display = "";
       }
     }
-  });
+});
 
 /* jQuery Search Filter */
 
